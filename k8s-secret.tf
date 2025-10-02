2b7b283b1ef86cace83b020fefc4750c3b58d92f
MarjoryMatos <marjory_matos@hotmail.com>
2025-10-02 14:57:56 -0300

    fix: ajuste em credenciais aws

--- k8s-secret.tf
+++ k8s-secret.tf
@@ -6,7 +6,20 @@ kind: Secret
 metadata:
   name: app-secrets
 type: Opaque
-stringData:
-##credenciais da aws
 YAML
 }
+
+resource "kubectl_manifest" "aws_credentials" {
+  depends_on = [aws_eks_cluster.cluster, aws_eks_node_group.node_group]
+  
+  yaml_body = <<YAML
+apiVersion: v1
+kind: Secret
+metadata:
+  name: aws-credentials
+type: Opaque
+stringData:
+  --inserir credenciais da aws para conexao com dynamodb
+YAML
+}
+
